6287e805add835f5ddbf7424644da122ee8c24e4
dock <chris.murdock8648@gmail.com>
2023-09-04 09:20:00 -0400

    fixing indexing

---
+++ database/migrations/update_category-meta-data.sql
@@ -0,0 +1,21 @@
+update categories set metaTitle='paintings', metaDescription='Browse our selection of fine antique oil, watercolour and pastel paintings. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=35;
+
+-- not done yet
+
+update categories set metaTitle='glass', metaDescription='Browse our online selection of fine antique and vintage art glass. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=36;
+update categories set metaTitle='jewelry', metaDescription='Browse our online selection of fine antique and vintage jewelry.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=37;
+update categories set metaTitle='sculptures', metaDescription='Browse our online selection of fine antique sculptures.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=38;
+update categories set metaTitle='furniture', metaDescription='Browse our online selection of fine antique furniture.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=39;
+update categories set metaTitle='objet d''art', metaDescription='Browse our online selection of fine objet d’art from around the world.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=46;
+update categories set metaTitle='ceramics', metaDescription='Browse our online selection of fine ceramics from the 18th century to present. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=41;
+update categories set metaTitle='porcelain', metaDescription='Browse our online selection of fine porcelain from the 18th century to present. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=42;
+update categories set metaTitle='silver', metaDescription='Browse our online selection of fine silver from the Georgian Era to present. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=34;
+update categories set metaTitle='clocks', metaDescription='Browse our online selection of fine clocks from the 18th century to present. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=8;
+update categories set metaTitle='new arrivals', metaDescription='Browse our online new arrivals section to see our newest acquisitions.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=1;
+update categories set metaTitle='featured antiques', metaDescription='Browse our online selection of featured antiques focusing on a period or style. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=45;
+update categories set metaTitle='seasonal antiques', metaDescription='Browse through our online selection of fine seasonal antiques. Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=44;
+update categories set metaTitle='curiosities', metaDescription='Browse through our online selection of curiosities and unusual finds.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=47;
+update categories set metaTitle='sold', metaDescription='Browse through our online selection of previously sold works.  Visit us in store at our gallery in Ambleside West Vancouver. We ship worldwide.' where id=33;
+
+
+
